2d42badf744d11fe39277c68a7643557e879d138
betul <bet.er@hotmail.com>
2020-07-15 20:17:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const people = [];
+const eventType = "";
+const thanksMessage = [];
+
+function writeCards(people, eventType) {
+    for(let i=0; i<people.length; i++) {
+        thanksMessage.push(`Thank you, ${people[i]}, for the wonderful ${eventType} gift!`);
+    }
+    return thanksMessage;
+}
+
+//writeCards(['Lisa', 'Kaitlin', 'Jan'], "surprise");
+
+let startFrom;
+
+function countDown(startFrom) {
+    while (startFrom >= 0) {
+        console.log(startFrom--);
+    }
+}
+
+countDown(10);
+
