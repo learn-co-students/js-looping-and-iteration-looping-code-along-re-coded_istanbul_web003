c0732e9a248f19e5dbd307900d36eab11c041ebd
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-07-20 05:32:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(a,b){
+    const array = []
+    for(let i = 0;i<a.length;i++){
+        array.push("Thank you, " +a[i]+ ", for the wonderful " +b+ " gift!");
+    }
+    return(array);
+}
+
+
+function countDown(c){
+while(c>=0){
+    console.log(c);
+    c-=1;
+}
+}
