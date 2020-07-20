5d2f2ca424b81e03aa5b860c0ec72b6307d93f40
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-07-20 14:19:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+  let card = [];
+  for (let i=0; i<names.length;i++){
+    card.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return card;
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+
+function countDown() {
+  let num=10;
+  while (0<=num && num<11) {
+    console.log(num--);
+  };
+  };
+  
+  countDown();
+
