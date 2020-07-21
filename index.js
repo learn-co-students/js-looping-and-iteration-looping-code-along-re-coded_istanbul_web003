f9c20193f7e17cec0e34e75c796ce5b2c127d756
Knar Gocer <knargr@gmail.com>
2020-07-21 09:31:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, evtname){
+  let counter=0;
+  const message=[];
+  for(let i=0; i<name.length;i++){
+    counter++;
+    message.push("Thank you, " + name[i]+", for the wonderful surprise gift!");
+  }
+  return message;
+}
+
+function countDown(int){
+  while(int>=0){
+    console.log(int);
+    int--;
+  }
+}
