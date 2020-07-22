fd99b4ef98b53d17cb9a33959c4ecf3fbfbf9b21
Ahmad Aljalmoud <github email address>
2020-07-23 02:37:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(listFriends, celebratedDay){
+    let gratitudeCard = [];
+    for (let i=0; i<listFriends.length;i++){
+      gratitudeCard.push(`Thank you, ${listFriends[i]}, for the wonderful ${celebratedDay} gift!`);
+    }
+
+    return gratitudeCard;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+function countDown(DD){
+    while (DD >=0) {
+        console.log(DD);
+        DD--;
+        
+    }
+
+}
+countDown(10);
