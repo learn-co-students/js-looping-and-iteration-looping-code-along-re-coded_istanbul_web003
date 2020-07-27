46616c195beb4b483b4c4cb912d50697dc80da98
gizemcdeniz <gizemc.deniz@gmail.com>
2020-07-27 22:17:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+/*
+function wraftgift() {
+  const cards = [["Ada", "Brendan", "Ali"], "birthday"];
+  for(let i = 0; i < cards.length; i++)
+  console.log(cards);
+}
+
+writeCards();
+*/
+
+const cards = [[ 'Lisa', 'Kaitlin', 'Jan' ], ['surprise']];
+ 
+function writeCards(cards) {
+  let array = [];
+  for (let i = 0; i < cards.length; i++) {
+    array[i] = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+     
+} 
+return array;
+}
+writeCards(cards); 
+
+function countDown(number) {
+  
+  while(number>-1) {
+    console.log(number)
+    number--;
+  }
+}
+
+countDown(10);
