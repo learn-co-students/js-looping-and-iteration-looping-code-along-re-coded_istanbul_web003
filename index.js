ec5c37fa12a1d426a0a9c4f5c5279bca87a970e4
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-10-20 16:02:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards (arr,str) {
+    let resultArr = []
+    for (let i=0; i<arr.length; i++) {
+        resultArr.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`)
+    }
+    return resultArr
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown (x) {
+    let i = x
+    while(i>=0){
+        console.log(i);
+        i--
+    }
+}
+
+countDown(10)
